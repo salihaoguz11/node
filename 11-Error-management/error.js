d67a2541e26d70e7aa14ebf1c60fb738653d4c18
Saliha Oguz <118985690+salihaoguz11@users.noreply.github.com>
2024-03-03 13:15:02 +0000

    htrow error

--- 11-Error-management/error.js
+++ 11-Error-management/error.js
@@ -5,14 +5,29 @@
 //*nodemon error.toJSON()
 
 "use strict";
-/* -------------------------------------------------------
+/* -------------------------------------------------------/
     EXPRESSJS - ERROR MANAGEMENT
-
     ------------------------------------------------------- */
 const express = require("express");
 const app = express();
 
 require("dotenv").config();
 const PORT = process.env.PORT || 8000;
+
+/* ------------------------------------------------------- */
+app.get("/", (req, res) => {
+  // console.log(abc)
+  //burada ust satiri yazinca hata verecek ve terminal dolacak ve hata thunder da html olarak gelecek.
+  throw new Error("Error Message");
+  //sistemin kendi kendine hata vermesini istemeyiz.Bunu bizim istedigimiz sekilde ayarlamamiz gerekiyor.
+  res.send("Hello");
+});
+
+/* ------------------------------------------------------- *
+/* ------------------------------------------------------- *
+/* ------------------------------------------------------- *
+/* ------------------------------------------------------- *
+/* ------------------------------------------------------- *
+/* ------------------------------------------------------- */
 
 app.listen(PORT, () => console.log("Running: http://127.0.0.1:" + PORT));
