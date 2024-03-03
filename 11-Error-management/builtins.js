2030ec797249b8e6777147008de76fc16092f8c9
Saliha Oguz <118985690+salihaoguz11@users.noreply.github.com>
2024-03-03 17:27:54 +0000

    static

--- 11-Error-management/builtins.js
+++ 11-Error-management/builtins.js
@@ -18,15 +18,18 @@ app.use(express.text());
 //? Accept Form-Data:
 // extended must be define. (if true, it supported nested form-data.)
 app.use(express.urlencoded({ extended: true }));
-//Bu kod olmadan form datayi alamaz.
+//Bu kod olmadan form datayi alamaz.Array'i  algilamasi icin true yapiyoruz.
 
 //* Allow staticfiles:
 // Call staticFiles from real-path
+//Css ve static JS dosyalari,resimleri kolaylikla yollayamam.
+//Bu tur dosayalri express/e bildirmem lazim.
 app.use("/static", express.static("./public/images"));
+// http://127.0.0.1:8000/static/monalisa.jpg -bunu taraciya yazinca resimi goruruz.
 
 app.all("/", (req, res) => {
   res.send({
-    body: req.body, // bu sekidle gonderilmis bir veriyi alabiliyorum.
+    body: req.body, // bu sekilde gonderilmis bir veriyi alabiliyorum.
     message: "Hello",
   });
 });
