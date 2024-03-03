8b46207f797a36d47d81caf8fee3c1d37d75a8fd
Saliha Oguz <118985690+salihaoguz11@users.noreply.github.com>
2024-03-03 13:24:35 +0000

    try-catch

--- 11-Error-management/error.js
+++ 11-Error-management/error.js
@@ -14,20 +14,54 @@ const app = express();
 require("dotenv").config();
 const PORT = process.env.PORT || 8000;
 
-/* ------------------------------------------------------- */
+/* ------------------------------------------------------- *
 app.get("/", (req, res) => {
   // console.log(abc)
   //burada ust satiri yazinca hata verecek ve terminal dolacak ve hata thunder da html olarak gelecek.
-  throw new Error("Error Message");
+  //   throw new Error("Error Message");
   //sistemin kendi kendine hata vermesini istemeyiz.Bunu bizim istedigimiz sekilde ayarlamamiz gerekiyor.
   res.send("Hello");
 });
 
+/* ------------------------------------------------------- */
+
 /* ------------------------------------------------------- *
+app.get("/user/:id", (req, res) => {
+  const id = req.params.id || 0; //id d'yi req icerisinden aliyoruz.
+  if (isNaN(id)) {
+    throw new Error("ID is not a number");
+  } else {
+    res.send({
+      message: "OK",
+      id,
+    });
+  }
+});
+
+/* ------------------------------------------------------- */
+
+//?sistemin istedigimiz gibi hata verm,esi icin try-catch kullanabiliriz
+/* ------------------------------------------------------- */
+app.get("/user/:id", (req, res) => {
+  const id = req.params.id || 0; //id d'yi req icerisinden aliyoruz.
+  try {
+    if (isNaN(id)) {
+      throw new Error("ID is not a number");
+    } else {
+      res.send({
+        message: "OK",
+        id,
+      });
+    }
+  } catch (err) {}
+});
+
 /* ------------------------------------------------------- *
-/* ------------------------------------------------------- *
-/* ------------------------------------------------------- *
-/* ------------------------------------------------------- *
+/* ------------------------------------------------------- */
+/* ------------------------------------------------------- */
+/* ------------------------------------------------------- */
+/* ------------------------------------------------------- */
+/* ------------------------------------------------------- */
 /* ------------------------------------------------------- */
 
 app.listen(PORT, () => console.log("Running: http://127.0.0.1:" + PORT));
