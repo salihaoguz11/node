2ad9988cc9999d1a8f520bb43d9b0f83c4ad3dee
Saliha Oguz <118985690+salihaoguz11@users.noreply.github.com>
2024-03-07 14:45:59 +0000

    findbyPk

---
+++ 13-Models-Todo2/app.js
@@ -0,0 +1,147 @@
+"use strict";
+/* -------------------------------------------------------
+    EXPRESSJS - TODO Project with Sequelize
+------------------------------------------------------- */
+
+const express = require("express");
+const app = express();
+
+require("dotenv").config();
+const PORT = process.env.PORT || 8000;
+
+/* ------------------------------------------------------- */
+
+// Accept json data:
+app.use(express.json());
+
+// Catch async-errors:
+require("express-async-errors");
+
+// app.all("/abc", (req, res) => {
+//   // Allow all methods. all -> URL=/ - use -> URL=/*
+//   res.send("WELCOME TO TODO API");
+// });
+
+/* ------------------------------------------------------- */
+//*Models
+const { Sequelize, DataTypes } = require("sequelize");
+// sequelize instance oluştur:
+const sequelize = new Sequelize("sqlite:./db.sqlite3");
+
+// define methodu sequelize modeli oluşturur:
+// her bir model, veritabanında bir tabloya denk gelir.
+// sequelize.define('tableName', {  modelDetails  })
+
+const Todo = sequelize.define("todos", {
+  //Burada Todo=> Model ismi,Genelde buyuk harf olur.
+
+  // ilk sutun olarak id sutunu sequelize tarafından otomatik oluşturulur/yönetilir.
+  // id: {
+  //     type: DataTypes.INTEGER,
+  //     allowNull: false, // default: true
+  //     unique: true, // default: false
+  //     comment: 'description',
+  //     primaryKey: true, // default: false
+  //     autoIncrement: true, // default: false
+  //     field: 'custom_name',
+  //     defaultValue: 'default', // default: null
+  // },
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+
+  description: DataTypes.TEXT, // ShortHand Using.
+
+  priority: {
+    // -1: Low, 0: Norm, 1: High
+    type: DataTypes.TINYINT,
+    allowNull: false,
+    default: 0,
+  },
+
+  isDone: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+    default: false,
+  },
+  //? Not need define createdAt & updatedAt fields.
+  //? createdAt ve updatedAt tanımlamaya gerek yoktur. Sequelize otomatik oluşturur/yönetir.
+});
+
+// Syncronization:
+// Model bilgilerini db'ye uygula:
+// sequelize.sync(); // CREATE TABLE - isin bitince kapat. Bunu actigin an sana db.sqlite3 file olusturur.
+// sequelize.sync({ force: true }) // DROP TABLE & CREATE TABLE
+// sequelize.sync({ alter: true }) // TO BACKUP & DROP TABLE & CREATE TABLE & FROM BACKUP
+
+// Connect to db: Terminalde yazilar degisti.
+sequelize
+  .authenticate()
+  .then(() => console.log("* DB Connected *"))
+  .catch(() => console.log("* DB Not Connected *"));
+/* ------------------------------------------------------- */
+
+/* ------------------------------------------------------- */
+
+const router = express.Router();
+
+// LIST TODOS:
+router.get("/", async (req, res) => {
+  // const data = await Todo.findAll()
+  const data = await Todo.findAndCountAll();
+
+  res.status(200).send({
+    error: false,
+    result: data,
+  });
+});
+
+//? CRUD Processes:
+
+// CREATE TODO:
+router.post("/", async (req, res) => {
+  // const receivedData = req.body (senin body kismina yazdigin veri)
+
+  // const data = await Todo.create({
+  //     title: receivedData.title,
+  //     description: receivedData.description,
+  //     priority: receivedData.priority,
+  //     isDone: receivedData.isDone,
+  //     // newKey: 'newValue' // Modelde tanımlanmadığı için bir işe yaramayacaktır.
+  // })
+
+  const data = await Todo.create(req.body);
+
+  res.status(201).send({
+    error: false,
+    result: data.dataValues,
+  });
+});
+// READ TODO:
+router.get("/:id", async (req, res) => {
+  // const data = await Todo.findOne({ where: { id: req.params.id } })
+  const data = await Todo.findByPk(req.params.id);
+
+  res.status(200).send({
+    error: false,
+    result: data, // veri tabanindan donen veridir.
+  });
+});
+
+app.use(router);
+/* ------------------------------------------------------- */
+const errorHandler = (err, req, res, next) => {
+  const errorStatusCode = res.errorStatusCode ?? 500;
+  console.log("errorHandler worked.");
+  res.status(errorStatusCode).send({
+    error: true, // special data
+    message: err.message, // error string message
+    cause: err.cause, // error option cause
+    // stack: err.stack, // error details
+  });
+};
+app.use(errorHandler);
+
+/* ------------------------------------------------------- */
+app.listen(PORT, () => console.log("Running: http://127.0.0.1:" + PORT));
--- 13-Models-Todo2/readme.md
+++ 13-Models-Todo2/readme.md
@@ -1,3 +1,46 @@
+# Model - Todo 2
+
+```jsx
 npm init -y
 npm i express dotenv express-async-errors
 gitignore
+nodemon app.js
+npm install sequelize sqlite3
+
+const express = require("express");
+const app = express();
+Burada express cagiriyorum ve express moturunu calistiriyorum.
+Daha sonra burada ki objeyi app'e atiyorum
+
+app.use(express.json())
+Data gonderirken(POST) JSON olarak gonderecegim diye express'e bildiriyorsun.
+```
+
+## ROUTERS
+
+```jsx
+const router = express.Router(); //routers larin ustune yaz.
+app.use(router); //routers larin en altina  yaz.
+
+findByPk(); //Primary key yani id ye gore getirir.
+findOne(); // Buldugu ilk kayidi getirir.
+findAndCountAll(); // Hem veri sayisini hemde verileri dondurur.
+```
+
+##
+
+```jsx
+
+```
+
+##
+
+```jsx
+
+```
+
+##
+
+```jsx
+
+```
