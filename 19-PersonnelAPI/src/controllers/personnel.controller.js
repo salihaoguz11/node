78752ed88267f27a8b0acdfc8bf284142d71e336
Saliha Oguz <118985690+salihaoguz11@users.noreply.github.com>
2024-03-19 00:38:06 +0000

    personnel router completed

--- 19-PersonnelAPI/src/controllers/personnel.controller.js
+++ 19-PersonnelAPI/src/controllers/personnel.controller.js
@@ -1,31 +1,92 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     EXPRESS - Personnel API
 ------------------------------------------------------- */
-const Personnel =require("../models/personnel.model")
+const Personnel = require("../models/personnel.model");
 
-module.exports={
-    list:async(req,res)=>{
-        const data = await res.getModelList(Personnel,departmentId)
+module.exports = {
+  list: async (req, res) => {
+    const data = await res.getModelList(Personnel, departmentId);
+    res.status(200).send({
+      error: false,
+      detail: await res.getModelListDetails(Personnel),
+      data,
+    });
+  },
+
+  create: async (req, res) => {
+    const data = await Personnel.create(req.body);
+    res.status(201).send({
+      error: false,
+      data,
+    });
+  },
+
+  read: async (req, res) => {
+    const data = await Personnel.findOne({ _id: req.params.id });
+    res.status(200).send({
+      error: false,
+      data,
+    });
+  },
+
+  update: async (req, res) => {
+    const data = await Personnel.updateOne({ _id: req.params.id }, req.body, {
+      runValidators: true,
+    });
+    res.status(202).send({
+      error: false,
+      data,
+      new: await Personnel.findOne({ _id: req.params.id }),
+    });
+  },
+
+  delete: async (req, res) => {
+    const data = await Personnel.deleteOne({ _id: req.params.id });
+
+    res.status(data.deletedCount ? 204 : 404).send({
+      error: !data.deletedCount,
+      data,
+    });
+  },
+  // LOGIN & LOGOUT
+
+  login: async (req, res) => {
+    const { username, password } = req.body;
+
+    if (username && password) {
+      const user = await Personnel.findOne({ username, password });
+      if (user) {
+        // Set Session:
+        req.session = {
+          id: user._id,
+          password: user.password,
+        };
+        // Set Cookie:
+        if (req.body?.rememberMe) {
+          req.sessionOptions.maxAge = 1000 * 60 * 60 * 24 * 3; // 3 Days
+        }
+
         res.status(200).send({
-            error:false,
-            detail:await res.getModelListDetails(Personnel),
-            data
-        })
-    },
-
-    create:async(req,res)=>{
-        const data =await Personnel.create(req.body)
-        res.status(201).send({
-            error:false,
-            data
-        })
+          error: false,
+          user,
+        });
+      } else {
+        res.errorStatusCode = 401;
+        throw new Error("Wrong Username or Password.");
+      }
+    } else {
+      res.errorStatusCode = 401;
+      throw new Error("Please entry username and password.");
     }
+  },
 
-
-
-
-    read:
-    update:
-    delete:
-}
+  logout: async (req, res) => {
+    // Set session to null:
+    req.session = null;
+    res.status(200).send({
+      error: false,
+      message: "Logout: Sessions Deleted.",
+    });
+  },
+};
